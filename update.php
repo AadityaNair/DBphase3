7ece3d2a3c264a1aca7cf46672de294b0f2f2567
Aaditya M Nair <aadityanair6494@gmail.com>
2014-11-13 16:17:11 +0530

    Updated all update queries to the primary ids.

--- update.php
+++ update.php
@@ -6,17 +6,18 @@
 
 
 if (isset($_POST['update_medicine'])) {  // Update Medicines to Database
-	$medicine_name=$_POST['name']; // Assumed Candidate Key
+    $id=$_POST['id'];
+    $medicine_name=$_POST['name']; // Assumed Candidate Key
 	$medicine_supplier=$_POST['supplier'];
 	$medicine_cost=$_POST['cost'];
 
 	// Now the sql
-	$query=$conn->prepare("UPDATE `Medicine` SET`supplier`=?, `cost`=? WHERE  `name`= ?);
+	$query=$conn->prepare("UPDATE `Medicine` SET `supplier`=?, `cost`=?, `name`= ? WHERE `id`=? ");
 	if (!$query){
 		echo "Update Unsuccessful";
 	}
 	else{
-		$query->bind_param("sis", $medicine_supplier, $medicine_cost, $medicine_name);
+		$query->bind_param("sisd", $medicine_supplier, $medicine_cost, $medicine_name, $id);
 		$query->execute();
 	}
 	$query->close();
@@ -25,17 +26,18 @@ if (isset($_POST['update_medicine'])) {  // Update Medicines to Database
 }
 
 if (isset($_POST['update_supplier'])) {  // Register a new supplier in database
-	$supplier_name=$_POST['name']; // Assumed Candidate Key
-    $supplier_phone=$_POST['phone];
+    $id=$_POST['id'];
+    $supplier_name=$_POST['name']; // Assumed Candidate Key
+    $supplier_phone=$_POST['phone'];
 	$supplier_address=$_POST['cost'];
 
 	// Now the sql
-	$query=$conn->prepare("UPDATE `Supplier` SET `phone`=?, `address`=? WHERE `name`=?");
+	$query=$conn->prepare("UPDATE `Supplier` SET `phone`=?, `address`=?, `name`=? WHERE `id`=? ");
 	if (!$query){
 		echo "Update Unsuccessful.";
 	}
 	else{
-		$query->bind_param("sds", $supplier_address, $supplier_phone, $supplier_name);
+		$query->bind_param("sdsd", $supplier_address, $supplier_phone, $supplier_name, $id);
 		$query->execute();
 	}
 	$query->close();
@@ -44,16 +46,17 @@ if (isset($_POST['update_supplier'])) {  // Register a new supplier in database
 }
 
 if (isset($_POST['update_to_inventory'])) {  // Update medicines to inventory.
-	$medicine_id=$_POST['med_id'];
+    $id=$_POST['id'];
+    $medicine_id=$_POST['med_id'];
 	$quantity_left=$_POST['quantity'];
 
 	// Now the sql
-	$query=$conn->prepare("UPDATE `Inventory` SET `quantity_left`=? WHERE `medicine_id`=?");
+	$query=$conn->prepare("UPDATE `Inventory` SET `quantity_left`=?, `medicine_id`=? WHERE `id`=? ");
 	if (!$query){
 		echo "Update Unsuccessful.";
 	}
 	else{
-		$query->bind_param("ds",$quantity_left, $medicine_id);
+		$query->bind_param("dsd",$quantity_left, $medicine_id, $id);
 		$query->execute();
 	}
 	$query->close();
@@ -62,17 +65,18 @@ if (isset($_POST['update_to_inventory'])) {  // Update medicines to inventory.
 }
 
 if (isset($_POST['update_patients'])) {  // Register Patients
-	$patient_name=$_POST['name'];  // Candidate Key
+    $id=$_POST['id'];
+    $patient_name=$_POST['name'];  // Candidate Key
     $patient_phone=$_POST['phone'];
     $patient_address=$_POST['address'];
 
 	// Now the sql
-	$query=$conn->prepare("UPDATE `Patients` SET `phone`=?, `address`=? WHERE `name`= ?");
+	$query=$conn->prepare("UPDATE `Patients` SET `phone`=?, `address`=?, `name`= ? WHERE `id`=?" );
 	if (!$query){
 		echo "Update Unsuccessful.";
 	}
 	else{
-		$query->bind_param("dss", $patient_phone, $patient_address, $patient_name);
+		$query->bind_param("dssd", $patient_phone, $patient_address, $patient_name, $id);
 		$query->execute();
 	}
 	$query->close();
@@ -81,18 +85,19 @@ if (isset($_POST['update_patients'])) {  // Register Patients
 }
 
 if (isset($_POST['update_contact'])) {  // Register Patients' Contact
-	$contact_name=$_POST['name']; // Candidate Key
+    $id=$_POST['id'];
+    $contact_name=$_POST['name']; // Candidate Key
     $contact_phone=$_POST['phone'];
     $contact_address=$_POST['address'];
     $patient_id=$_POST['patient_id'];
 
 	// Now the sql
-	$query=$conn->prepare("UPDATE `Patient_Contact` SET `phone`=?, `address`=? WHERE `patient_id`=? AND `name`=?");
+	$query=$conn->prepare("UPDATE `Patient_Contact` SET `phone`=?, `address`=?, `patient_id`=?, `name`=? WHERE `id`=? ");
 	if (!$query){
 		echo "Update Unsuccessful.";
 	}
 	else{
-		$query->bind_param("dsds", $contact_phone, $contact_address, $patient_id, $contact_name);
+		$query->bind_param("dsdsd", $contact_phone, $contact_address, $patient_id, $contact_name, $id);
 		$query->execute();
 	}
 	$query->close();
@@ -101,16 +106,17 @@ if (isset($_POST['update_contact'])) {  // Register Patients' Contact
 }
 
 if (isset($_POST['update_employee'])) {  // Register Employee
-	$name=$_POST['name'];
+    $id=$_POST['id'];
+    $name=$_POST['name'];
 	$salary=$_POST['salary'];
 
 	// Now the sql
-	$query=$conn->prepare("UPDATE `Employee` SET `salary`=? WHERE `name`=?");
+	$query=$conn->prepare("UPDATE `Employee` SET `salary`=?, `name`=? WHERE `id`=? ");
 	if (!$query){
 		echo "Update Unsuccessful.";
 	}
 	else{
-        $query->bind_param("ds", $salary, $name)
+        $query->bind_param("dsd", $salary, $name, $id)
 		$query->execute();
     }
     // Code to link to create links to update_pharmacist / update_doctor
@@ -120,16 +126,17 @@ if (isset($_POST['update_employee'])) {  // Register Employee
 }
 
 if (isset($_POST['update_sale'])) {  // Register a Sale
-	$employee_id=$_POST['employee'];
+    $id=$_POST['id'];
+    $employee_id=$_POST['employee'];
 	$cost=$_POST['cost'];
 
 	// Now the sql
-	$query=$conn->prepare("UPDATE `Sales` SET `cost`=? WHERE `employee_id`=?");
+	$query=$conn->prepare("UPDATE `Sales` SET `cost`=?, `employee_id`=? WHERE `id`=? ");
 	if (!$query){
 		echo "Update Unsuccessful.";
 	}
 	else{
-		$query->bind_param("dd", $cost, $employee_id);
+		$query->bind_param("ddd", $cost, $employee_id, $id);
 		$query->execute();
 	}
 	$query->close();
@@ -139,17 +146,18 @@ if (isset($_POST['update_sale'])) {  // Register a Sale
 }
 
 if (isset($_POST['medicine_sold'])) {  // Store Medicines Sold
-	$sale_id=$_POST['sale_id'];
+    $id=$_POST['id'];
+    $sale_id=$_POST['sale_id'];
     $quantity=$_POST['quantity'];
     $medicine_id=$_POST['medicine_id'];
 
 	// Now the sql
-	$query=$conn->prepare("UPDATE `Medicine_Sold` SET `quantity`=? WHERE `medicine_id`=? AND `sale_id`=?");
+	$query=$conn->prepare("UPDATE `Medicine_Sold` SET `quantity`=?, `medicine_id`=?, `sale_id`=? WHERE `id`=? ");
 	if (!$query){
 		echo "Update Unsuccessful.";
 	}
 	else{
-		$query->bind_param("ddd", $quantity_left, $medicine_id, $medicine_id);
+		$query->bind_param("dddd", $quantity_left, $medicine_id, $medicine_id, $id);
 		$query->execute();
 	}
 	$query->close();
@@ -158,18 +166,19 @@ if (isset($_POST['medicine_sold'])) {  // Store Medicines Sold
 }
 
 if (isset($_POST['update_employee_dependent'])) {  // Update Employees' Contact
-	$dependent_name=$_POST['name'];
+    $id=$_POST['id'];
+    $dependent_name=$_POST['name'];
     $dependent_phone=$_POST['phone'];
     $dependent_address=$_POST['address'];
     $employee_id=$_POST['employee_id'];
 
 	// Now the sql
-	$query=$conn->prepare("UPDATE `Employee_Dependent` SET `phone`=?, `address`=? WHERE `employee_id`=? AND `name`=?");
+	$query=$conn->prepare("UPDATE `Employee_Dependent` SET `phone`=?, `address`=?, `employee_id`=?, `name`=? WHERE `id`=? ");
 	if (!$query){
 		echo "Update Unsuccessful.";
 	}
 	else{
-		$query->bind_param("dsds", $dependent_phone, $dependent_address, $employee_id, $dependent_name);
+		$query->bind_param("dsdsd", $dependent_phone, $dependent_address, $employee_id, $dependent_name, $id);
 		$query->execute();
 	}
 	$query->close();
@@ -178,17 +187,18 @@ if (isset($_POST['update_employee_dependent'])) {  // Update Employees' Contact
 }
 
 if (isset($_POST['update_pharmacist'])) {  // Pharmacist Specialisation
-	$employee_id=$_POST['employee_id'];
+    $id=$_POST['id'];
+    $employee_id=$_POST['employee_id'];
     $designation=$_POST['designation'];
     $department=$_POST['department'];
 
 	// Now the sql
-	$query=$conn->prepare("UPDATE `Pharmacist` SET `department`=?, `designation`=? WHERE `employee_id`=?");
+	$query=$conn->prepare("UPDATE `Pharmacist` SET `department`=?, `designation`=?, `employee_id`=? WHERE `id`=? ");
 	if (!$query){
 		echo "Update Unsuccessful.";
 	}
 	else{
-		$query->bind_param("ssd", $department, $designation, $employee_id);
+		$query->bind_param("ssdd", $department, $designation, $employee_id, $id);
 		$query->execute();
 	}
 	$query->close();
@@ -197,17 +207,18 @@ if (isset($_POST['update_pharmacist'])) {  // Pharmacist Specialisation
 }
 
 if (isset($_POST['update_doctor'])) {  // Doctor Specialisation
-	$employee_id=$_POST['employee_id'];
+    $id=$_POST['id'];
+    $employee_id=$_POST['employee_id'];
     $designation=$_POST['designation'];
     $department=$_POST['department'];
 
 	// Now the sql
-	$query=$conn->prepare("UPDATE `Doctor` SET `department`=?, `designation`=? WHERE `employee_id`=?");
+	$query=$conn->prepare("UPDATE `Doctor` SET `department`=?, `designation`=?, `employee_id`=? WHERE `id`=? ");
 	if (!$query){
 		echo "Update Unsuccessful.";
 	}
 	else{
-		$query->bind_param("ssd", $department, $designation, $employee_id);
+		$query->bind_param("ssdd", $department, $designation, $employee_id, $id);
 		$query->execute();
 	}
 	$query->close();
@@ -216,16 +227,17 @@ if (isset($_POST['update_doctor'])) {  // Doctor Specialisation
 }
 
 if (isset($_POST['update_ayurvedic'])) {  // Ayurvedic
-	$medicine_id=$_POST['medicine_id'];
+    $id=$_POST['id'];
+    $medicine_id=$_POST['medicine_id'];
 	$type=$_POST['type'];
 
 	// Now the sql
-	$query=$conn->prepare("UPDATE `Ayurvedic` SET `type`=? WHERE `medicine_id`=?");
+	$query=$conn->prepare("UPDATE `Ayurvedic` SET `type`=?, `medicine_id`=? WHERE `id`=? ");
 	if (!$query){
 		echo "Update Unsuccessful.";
 	}
 	else{
-		$query->bind_param("sd", $type, $medicine_id);
+		$query->bind_param("sdd", $type, $medicine_id, $id);
 		$query->execute();
 	}
 	$query->close();
@@ -234,16 +246,17 @@ if (isset($_POST['update_ayurvedic'])) {  // Ayurvedic
 }
 
 if (isset($_POST['update_sedative'])) {  // Sedative
-	$medicine_id=$_POST['medicine_id'];
+    $id=$_POST['id'];
+    $medicine_id=$_POST['medicine_id'];
 	$type=$_POST['type'];
 
 	// Now the sql
-	$query=$conn->prepare("UPDATE `Sedative` SET `type`=? WHERE `medicine_id`=?");
+	$query=$conn->prepare("UPDATE `Sedative` SET `type`=?, `medicine_id`=? WHERE `id`=? ");
 	if (!$query){
 		echo "Update Unsuccessful.";
 	}
 	else{
-		$query->bind_param("sd", $type, $medicine_id);
+		$query->bind_param("sdd", $type, $medicine_id, $id);
 		$query->execute();
 	}
 	$query->close();
@@ -252,16 +265,17 @@ if (isset($_POST['update_sedative'])) {  // Sedative
 }
 
 if (isset($_POST['update_homeopathic'])) {  // Homeopathic
-	$medicine_id=$_POST['medicine_id'];
+    $id=$_POST['id'];
+    $medicine_id=$_POST['medicine_id'];
 	$type=$_POST['type'];
 
 	// Now the sql
-	$query=$conn->prepare("UPDATE `Homeopathic` SET `type`=? WHERE `medicine_id`=?");
+	$query=$conn->prepare("UPDATE `Homeopathic` SET `type`=?, `medicine_id`=? WHERE `id`=? ");
 	if (!$query){
 		echo "Update Unsuccessful.";
 	}
 	else{
-		$query->bind_param("sd", $type, $medicine_id);
+		$query->bind_param("sdd", $type, $medicine_id, $id);
 		$query->execute();
 	}
 	$query->close();
@@ -270,21 +284,21 @@ if (isset($_POST['update_homeopathic'])) {  // Homeopathic
 }
 
 if (isset($_POST['update_miscellaneous'])) {  // Miscellaneous
-	$medicine_id=$_POST['medicine_id'];
+    $id=$_POST['id'];
+    $medicine_id=$_POST['medicine_id'];
 	$type=$_POST['type'];
 
 	// Now the sql
-	$query=$conn->prepare("UPDATE `Miscellaneous` SET `type`=? WHERE `medicine_id`=?");
+	$query=$conn->prepare("UPDATE `Miscellaneous` SET `type`=?, `medicine_id`=? WHERE `id`=? ");
 	if (!$query){
 		echo "Update Unsuccessful.";
 	}
 	else{
-		$query->bind_param("sd", $type, $medicine_id);
+		$query->bind_param("sdd", $type, $medicine_id, $id);
 		$query->execute();
 	}
 	$query->close();
 	$conn->close();
 	exit;
 }
-
 ?>
